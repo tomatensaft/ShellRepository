85425959e932548885ce0a3236505c5725bab02d
tomatensaft <marco_unterberger@gmx.at>
2022-07-19 22:10:29 +0200

    bhyve updte

--- bhyve/bhyve_debian_guest.sh
+++ bhyve/bhyve_debian_guest.sh
@@ -5,77 +5,90 @@
 #Install Debian Bhyve Guest
 
 #Print Header
-printf "-----------------------------"
-printf "|                           |"
-printf "|  Exmaple Bhyve Debian     |"
-printf "|  Adjust Params in Script  |"
-printf "|                           |"
-printf "-----------------------------"
+printf "|---------------------------|\n"
+printf "|                           |\n"
+printf "|  Exmaple Bhyve Debian     |\n"
+printf "|                           |\n"
+printf "|                           |\n"
+printf "|---------------------------|\n"
 
-#Check Root
-if [ $(id -u) -ne 0 ]; then
-	printf "Usage: run '$0' as root.\n"
-	return 1
-fi
-
-#Check Bhyve
-if command -v bhyve >/dev/null 2>&1 ; then
-    printf "Bhyve Grub Program Found...\n"
-else
-    printf "Bhyve Grub Program Not Found...\n"
-    exit 1
-fi 
-
-#Check Grub Uefi Loader
-if pkg info grub2-bhyve | grep bhyve; then
-    printf "Bhyve Grub Uefi Loader Found...\n"
-else
-    pkg install -y grub2-bhyve 
-fi
-
-#Check Kernel Modules
-if kldstat | grep vmm; then
-    printf "Kernel Module vmm found"
-else
-    printf "Load Kernel module vmm"
-    kldload vmm
-    #exit 1
-fi
-
-#Params - Adjust if needed
+#Static Params - Adjust if needed / Init
 iso_file="$(pwd)/debian-installer.iso"
-debian_file= "debian-11.4.0-amd64-netinst.iso"
+debian_file="debian-11.4.0-amd64-netinst.iso"
 vm_disk="zroot/debian-vm_disk0"
 vnc_port="5900"
 nic="wlan0" 
 
+#Include extenal scripts
+. bhyve_net_config.sh
+
+#Main Functions
+main_bhyve_debian_guest() {
+    #Check Inputargs
+    case $1 in
+            --test)
+                printf "Test Command for debugging $0\n"
+                main_bhyve_net_config --test
+                ;;
+
+            --create)
+                printf "Try create Bhyve Debian guest\n"
+                check_requirements
+                main_bhyve_net_config --create
+                load_iso
+                create_zfs_disk 
+                install_debian_guest
+                ;;
+
+            --start)
+                printf "Try start Bhyve Debian guest\n"
+                check_requirements
+                start_debian_guest
+                ;;
+
+
+            --delete)
+                printf "Try delete Bhyve Debian guest\n"
+                main_bhyve_net_config --delete
+                delete_debian_guest
+                ;;
+
+            --info)
+                printf "Get Status of Bhyve Debian Guest\n"
+                clear
+                ;;
+            *)
+                printf "Invalid option!\n\n"
+                usage      
+                ;;
+    esac
+}
+
+
 #Get IsoFiles
-printf "Fetch Debian Iso File ${debian_file}..\n"
-fetch -o "${iso_file}" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/${debian_file}"
+load_iso() {
+    printf "Fetch Debian Iso File ${debian_file}..\n"
+    if ls | grep installer.iso; then #Any Installer Iso
+        printf "File found allready ${debian_file}..\n"
+    else
+        fetch -o "${iso_file}" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/${debian_file}"
+    fi
+}
 
 #Check zroot disk exists
-if zfs list | grep vm_disk; then
-    printf "zfs disk found"
-else    
-    printf "zfs create disk ${vm_disk}"    
-    zfs create -V 10G -o volmode=dev ${vm_disk}
-fi
-
-#Load Nic
-. bhyve_net_config.sh $nic
-if [ $? -eq 1]; then
-    printf "Problem Creating Nic Bridge"
-    exit 1
-else
-    create_bridge    
-fi
-
-return 0;
+create_zfs_disk() {
+    if zfs list | grep ${vm_disk}; then
+        printf "zfs disk found\n"
+    else    
+        printf "zfs create disk ${vm_disk}"    
+        zfs create -V 10G -o volmode=dev ${vm_disk}
+    fi
+}
 
 #Start Bhyve Debian Guest
-#2 Cores / 2 GigRAM / VirtIO / Uefi
 install_debian_guest() 
 {
+    #2 Cores / 2 GigRAM / VirtIO / Uefi
     bhyve -c 2 -m 2G \
     -A -H \
     -l bootrom,/usr/local/share/uefi-firmware/BHYVE_BHF_UEFI.fd \
@@ -89,6 +102,7 @@ install_debian_guest()
     debian-vm
 }
 
+#Start Bhyve Debian Guest
 start_debian_guest() 
 {
 doas bhyve -c 2 -m 2G \
@@ -104,8 +118,54 @@ doas bhyve -c 2 -m 2G \
 debian-vm
 }
 
-destroy_debian_guest() 
+#Delete Bhyve Debian Guest
+delete_debian_guest() 
 {
     bhyvectl --vm=debian-vm --destroy
 }
 
+
+#Check requirements
+check_requirements()
+{
+    #Check Root
+    if [ $(id -u) -ne 0 ]; then
+        printf "Usage: run '$0' as root.\n"
+        exit 1
+    fi
+
+    #Check Bhyve
+    if command -v bhyve >/dev/null 2>&1 ; then
+        printf "Bhyve Grub Program Found...\n"
+    else
+        printf "Bhyve Grub Program Not Found...\n"
+        exit 1
+    fi 
+
+    #Check Grub Uefi Loader
+    if pkg info grub2-bhyve | grep bhyve; then
+        printf "Bhyve Grub Uefi Loader Found...\n"
+    else
+        pkg install -y grub2-bhyve 
+    fi
+
+    #Check Kernel Modules
+    if kldstat | grep vmm; then
+        printf "Kernel Module vmm found"
+    else
+        printf "Load Kernel module vmm"
+        kldload vmm
+    fi
+}
+
+
+#Usage
+usage() {
+    printf "$0 Usage: --"
+    printf "[test,create,start,stop,info,delete]\n\n"
+    exit 1
+}
+
+
+#Call main Function manualla - if not need uncomment
+main_bhyve_debian_guest "$@"; exit
--- bhyve/bhyve_net_config.sh
+++ bhyve/bhyve_net_config.sh
@@ -6,69 +6,140 @@
 printf "|---------------------------|\n"
 printf "|                           |\n"
 printf "|  Init Bhyve Net           |\n"
-printf "|  Parameter for NIC need   |\n"
+printf "|                           |\n"
 printf "|                           |\n"
 printf "|---------------------------|\n"
 
-#Check Root
-if [ $(id -u) -ne 0 ]; then
-	printf "Usage: run '$0' as root.\n"
-	return 1
-fi
+#Static Params - Adjust if needed / Init
+tap=$(ifconfig -a | sed 's/[ \t:].*//;/^$/d' | grep tap)
+bridge=$(ifconfig -a | sed 's/[ \t:].*//;/^$/d' | grep bridge)
 
-#Select NIC
-if [ ! -z $nic ]; then
-    printf "Nic Parameter found"
-else
-    #Check NIC Parameter
-    if [ -z $1  ]; then
-        printf "Parameter for NIC also Empty\n"
-        return 1
-    else
-        nic=$1
-        printf "Use NIC $nic\n" 
-    fi
-fi
+#Main Functions
+main_bhyve_net_config() {
+    #Check Inputargs
+    case $1 in
+            --test)
+                printf "Test Command for debugging $0\n"
+                ;;
+
+            --create)
+                printf "Try create Bhyve Network\n"
+                check_requirements
+                select_nic
+                check_nic
+                create_bridge_tap
+                create_firewall_rules
+                ;;
+
+            --start)
+                printf "Try start Bhyve Network\n"
+                check_requirements
+                start_debian_guest
+                ;;
+
+            --delete)
+                printf "Try delete Bhyve Newtork\n"
+                delete_bridge
+                ;;
+
+            --info)
+                    printf "Get Status of Bhyve Network\n"
+                    clear
+                    ;;
+            *)
+                    printf "Invalid option!\n\n"
+                    usage
+                    exit 1
+                    ;;
+    esac
+}
 
 
-#Params - Adjust if needed
-tap="$(ifconfig tap create)"
-bridge="$(ifconfig bridge create)"
+#Create Brigde
+create_bridge_tap() {
+    $tap="$(ifconfig tap create)"
+    $bridge="$(ifconfig bridge create)"
+}
 
-#Check if Nic Exist
-if ifconfig | grep $nic; then
-    printf "NIC ${nic} found"
-else
-    printf  "NIC ${nic} not found"
-    exit 1
-fi
 
-#Check if Bridge exists
-if ifconfig | grep bridge; then
-    printf "Bridge Allready Exists"
-    return 0
-fi
-
-#Create Brige
-create_bridge() {
-    printf "Try to create bridge"
+#Create Firewall Rules
+create_firewall_rules()
+{
+    printf "Try to create bridge\n"
     sysctl net.link.tap.up_on_open=1
     ifconfig ${bridge} addm ${tap} addm ${nic} up
     sysctl net.link.bridge.pfil_member=0
-    temp_firewall_rules="$(mktemp)"
-    echo "pass in quick on ${bridge}" >> "${temp_firewall_rules}"
-    echo "pass in quick proto tcp to port ${vnc_port}" >> "${temp_firewall_rules}"
-    pfctl -a bhf/bhyve/debian-vm -f "${temp_firewall_rules}"
+
+    #Firewall Rules for Beckhoff device
+    if uname -a | grep bhf; then
+        printf "Beckhoff device foudn - create firewall rules\n"
+        temp_firewall_rules="$(mktemp)"
+        echo "pass in quick on ${bridge}" >> "${temp_firewall_rules}"
+        echo "pass in quick proto tcp to port ${vnc_port}" >> "${temp_firewall_rules}"
+        pfctl -a bhf/bhyve/debian-vm -f "${temp_firewall_rules}"
+    else
+        printf "No Beckhoff device - no firewall rules\n"
+    fi
 }
 
 
 #Delete Bridge
 delete_bridge() {
-    printf "Try to delete bridge"
+    printf "Try to delete bridge\n"
     ifconfig ${bridge} deletem ${tap} deletem ${nic}
     ifconfig ${bridge} down
     ifconfig ${bridge} destroy
     ifconfig ${tap} destroy
-    pfctl -a bhf/bhyve/debian-vm -f /dev/null
-    #rm "${temp_firewall_rules}"
+    if uname -a | grep bhf; then
+        pfctl -a bhf/bhyve/debian-vm -f /dev/null
+        #rm "${temp_firewall_rules}"
+    fi
+    
 }
+
+
+check_nic() {
+    #Check if Nic Exist
+    if ifconfig | grep $nic; then
+        printf "NIC ${nic} found\n"
+    else
+        printf  "NIC ${nic} not found\n"
+        exit 1
+    fi
+}
+
+check_bridge(){
+    #Check if Bridge exists
+    if ifconfig | grep bridge; then
+        printf "Bridge Allready Exists\n"
+        exit 0
+    fi
+}
+
+select_nic(){
+    #Select NIC
+    if [ ! -z $nic ]; then
+        printf "Nic Parameter found\n"
+    else
+        #Check NIC Parameter
+        if [ -z $1  ]; then
+            printf "Parameter for NIC also Empty\n"
+            return 1
+        else
+            nic=$1
+            printf "Use NIC $nic\n" 
+        fi
+    fi
+}
+
+
+check_requirements() {
+    #Check Root
+    if [ $(id -u) -ne 0 ]; then
+        printf "Usage: run '$0' as root.\n"
+        return 1
+    fi
+}
+
+#Call main Function manualla - if not need uncomment - Lib Function
+#main_bhyve_net_config "$@"; exit
